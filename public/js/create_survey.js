4df23b84c3dcb92ee47ef45538c9064550667869
Adigo Atabo <Adigo@Adigos-MacBook-Air.local>
2013-10-19 17:58:28 -0500

    create survey veiw complete

--- public/js/create_survey.js
+++ public/js/create_survey.js
@@ -1,31 +1,33 @@
 $(document).ready(function () {
 
-  // event.preventDefault();
-
   var startingNo = 0;
   var $node = "";
 
   // append counter to identifiers
   for( varCount = 0; varCount <= startingNo; varCount ++ ) {
     var displayCount = varCount +1;
-    $node += '<p><label for="question'+displayCount+'">Question '+displayCount+': </label><input type="text" name="question'+displayCount+'" id="question'+displayCount+'"><span class="remove_question"><input type="submit" id="remove_question" value="Remove Question"</span></p>';
-    event.preventDefault();
+
+    // create first node
+    $node = '<p class="question_row"><label for="question'+displayCount+'">Question '+displayCount+': </label><input id="question'+displayCount+'" class="question_textbox" type="text" name="question'+displayCount+'" placeholder=" e.g., How often do you feel sleepy?"><input type="submit" id="remove_question" class="remove_question" value="Remove Question"<br></p>';
+
+    //add first node to the DOM
+    $('#add_question').before($node);
   }
-
-  //add them to the DOM
-  $('form').prepend($node);
-
-  //remove a textfield
-  $('#remove_question').on('click', function(){
-    event.preventDefault();
-    $(this).remove();
-  });
 
   //add a new node
   $('#add_question').on('click', function(){
     event.preventDefault();
+
     varCount++;
-    $node = '<p><label for="question'+varCount+'">Question '+varCount+': </label><input type="text" name="question'+varCount+'" id="question'+varCount+'"><span class="remove_question"><input type="submit" id="remove_question" value="Remove Question"</span></p>';
-    $(this).parent().before($node);
-  });
+    $node = '<p class="question_row"><label for="question'+varCount+'">Question '+varCount+': </label><input id="question'+varCount+'" class="question_textbox" type="text" name="question'+varCount+'"><input type="submit" id="remove_question" class="remove_question" value="Remove Question"><br></p>';
+
+    //add new node to the DOM
+    $("#add_question").before($node);
+
+    //remove a textfield
+    $('.remove_question').on('click', function(event){
+      event.preventDefault();
+      $(this).parent().remove();
+    })
+  })
 });
